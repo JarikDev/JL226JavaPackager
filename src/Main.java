df246c98b06301a81b7f3a8848d809037891773a
ivan4000 <sxln1@mail.ru>
2018-11-21 19:23:32 +0300

    JL226javaPackager

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group group=new Group();
+        Scene scene=new Scene(group,500,400);
+        primaryStage.setTitle("Hello World !!!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
